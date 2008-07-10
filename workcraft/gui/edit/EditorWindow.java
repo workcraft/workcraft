6e09985d0e8fdc14a60caba8ef359e078048f375
Ivan Poliakov <ivan.poliakov@ncl.ac.uk>
2008-07-10 08:53:16 +0000

    Routine commit

---
+++ workcraft/gui/edit/EditorWindow.java
@@ -0,0 +1,9 @@
+package org.workcraft.gui.edit;
+
+import org.workcraft.gui.InternalWindow;
+
+public abstract class EditorWindow extends InternalWindow {
+	public EditorWindow(String title) {
+		super(title);
+	}
+}
