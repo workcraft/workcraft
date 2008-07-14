cb2974af978aa8b78adca5fbde8b4fd00745b913
Ivan Poliakov <ivan.poliakov@ncl.ac.uk>
2008-07-14 08:55:38 +0000

    Routine commit.

--- workcraft/framework/exceptions/DuplicateIdException.java
+++
@@ -1,9 +0,0 @@
-package org.workcraft.framework.exceptions;
-
-public class DuplicateIdException extends Exception {
-	private static final long serialVersionUID = 1L;
-
-	public DuplicateIdException(Integer id) {
-		super(id.toString());
-	}
-}
