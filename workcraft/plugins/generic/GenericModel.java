1210fe99fb5a361f0b776440609b38980cc58ae5
Ivan Poliakov <ivan.poliakov@ncl.ac.uk>
2008-07-14 10:57:23 +0000

    Routine commit

--- workcraft/plugins/generic/GenericModel.java
+++ workcraft/plugins/generic/GenericModel.java
@@ -1,10 +1,12 @@
 package org.workcraft.plugins.generic;
 
+import org.workcraft.dom.ModelMeta;
 import org.workcraft.framework.Model;
 import org.workcraft.framework.Plugin;
 
+@ModelMeta (displayName = "Generic model")
 public class GenericModel extends Model {
-	public static final String CAPTION = "Generic model";
+	//public static final String CAPTION = "Generic model";
 
 	public static Plugin getInstance() {
 		return new GenericModel();
