0b6e372f394559dd20946a5b0c9434753b9b3524
Andrey Mokhov <andrey.mokhov@gmail.com>
2008-12-11 19:44:42 +0000

    Petri Visual Place added.

--- workcraft/plugins/petri/Place.java
+++ workcraft/plugins/petri/Place.java
@@ -3,11 +3,18 @@ package org.workcraft.plugins.petri;
 import org.w3c.dom.Element;
 import org.workcraft.dom.Component;
 import org.workcraft.dom.DisplayName;
+import org.workcraft.dom.VisualClass;
 
-@DisplayName ("Place")
+@DisplayName("Place")
+@VisualClass("org.workcraft.plugins.petri.VisualPlace")
 public class Place extends Component {
+
+	protected int tokens = 0;
+	private static int counter = 0;
+
 	public Place() {
 		super();
+		tokens = (counter++) % 10;
 	}
 
 	public Place(Element xmlElement) {
---
+++ workcraft/plugins/petri/VisualPlace.java
@@ -0,0 +1,91 @@
+package org.workcraft.plugins.petri;
+
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.Shape;
+import java.awt.geom.Ellipse2D;
+import java.awt.geom.Rectangle2D;
+
+import org.w3c.dom.Element;
+import org.workcraft.dom.visual.VisualComponent;
+
+public class VisualPlace extends VisualComponent {
+	private static double size = 1;
+	private static float strokeWidth = 0.1f;
+	private static double singleTokenSize = size / 2;
+	private static double multipleTokenSeparation = strokeWidth / 8;
+
+	public VisualPlace(Place place) {
+		super(place);
+	}
+
+	public VisualPlace(Place place, Element xmlElement) {
+		super(place, xmlElement);
+	}
+
+	@Override
+	public void draw(Graphics2D g)
+	{
+		Shape shape = new Ellipse2D.Double(
+				getX() - size / 2 + strokeWidth / 2,
+				getY() - size / 2 + strokeWidth / 2,
+				size - strokeWidth,
+				size - strokeWidth);
+
+		g.setColor(Color.WHITE);
+		g.fill(shape);
+		g.setColor(Color.BLACK);
+		g.setStroke(new BasicStroke(strokeWidth));
+		g.draw(shape);
+
+		Place p = (Place)getReferencedComponent();
+
+		if (p.tokens == 1)
+		{
+			shape = new Ellipse2D.Double(
+					getX() - singleTokenSize / 2,
+					getY() - singleTokenSize / 2,
+					singleTokenSize,
+					singleTokenSize);
+
+			g.setColor(Color.BLACK);
+			g.fill(shape);
+		}
+		else
+		if (p.tokens > 1 && p.tokens < 8)
+		{
+			double al = Math.PI / p.tokens;
+			if (p.tokens == 7) al = Math.PI / 6;
+
+			double r = (size / 2 - strokeWidth - multipleTokenSeparation) / (1 + 1 / Math.sin(al));
+			double R = r / Math.sin(al);
+
+			r -= multipleTokenSeparation;
+
+			for(int i = 0; i < p.tokens; i++)
+			{
+				if (i == 6)
+					shape = new Ellipse2D.Double(getX() - r, getY() - r, r * 2, r * 2);
+				else
+					shape = new Ellipse2D.Double(
+							getX() - R * Math.sin(i * al * 2) - r,
+							getY() - R * Math.cos(i * al * 2) - r,
+							r * 2,
+							r * 2);
+
+				g.setColor(Color.BLACK);
+				g.fill(shape);
+			}
+		}
+		else if (p.tokens > 7)
+		{
+	//		g.drawString(Integer.toString(p.tokens), (float)getX(), (float)getY());
+		}
+	}
+
+
+	public Rectangle2D getBoundingBox() {
+		return new Rectangle2D.Double(getX()-size/2, getY()-size/2, size, size);
+	}
+}
