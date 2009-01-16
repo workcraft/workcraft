ff50b0c4bd549351a8be1a09ac3543436aee6bcd
Ivan Poliakov <ivan.poliakov@ncl.ac.uk>
2009-01-16 15:47:06 +0000

    #14: VisualConnection bounding box is now calculated taking hitThreshold into account.

--- workcraft/dom/visual/VisualConnection.java
+++ workcraft/dom/visual/VisualConnection.java
@@ -191,7 +191,7 @@ public class VisualConnection extends VisualNode implements PropertyChangeListen
 	}
 
 	public int hitTestInParentSpace(Point2D pointInParentSpace) {
-		if (distanceToConnection(pointInParentSpace) < arrowWidth)
+		if (distanceToConnection(pointInParentSpace) < hitThreshold)
 			return 1;
 		else
 			return 0;
@@ -200,6 +200,19 @@ public class VisualConnection extends VisualNode implements PropertyChangeListen
 	public Rectangle2D getBoundingBoxInParentSpace() {
 		Rectangle2D bb = new Rectangle2D.Double(lineStart.getX(), lineStart.getY(), 0, 0);
 		bb.add(arrowHeadPosition);
+		Point2D lineVec = new Point2D.Double(arrowHeadPosition.getX() - lineStart.getX(), arrowHeadPosition.getY() - lineStart.getY());
+
+		double mag = lineVec.distance(0, 0);
+
+		if (mag==0)
+			return bb;
+
+		lineVec.setLocation(lineVec.getY()*hitThreshold/mag, -lineVec.getX()*hitThreshold/mag);
+		bb.add(lineStart.getX() + lineVec.getX(), lineStart.getY() + lineVec.getY());
+		bb.add(arrowHeadPosition.getX() + lineVec.getX(), arrowHeadPosition.getY() + lineVec.getY());
+		bb.add(lineStart.getX() - lineVec.getX(), lineStart.getY() - lineVec.getY());
+		bb.add(arrowHeadPosition.getX() - lineVec.getX(), arrowHeadPosition.getY() - lineVec.getY());
+
 		return bb;
 	}
 
