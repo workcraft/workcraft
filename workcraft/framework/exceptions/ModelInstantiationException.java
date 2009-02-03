11072642b59b0b4e8ba7e0c33b45142f489a5720
Ivan Poliakov <ivan.poliakov@ncl.ac.uk>
2009-02-03 23:44:54 +0000

    Added missing exception class.

---
+++ workcraft/framework/exceptions/ModelInstantiationException.java
@@ -0,0 +1,8 @@
+package org.workcraft.framework.exceptions;
+
+@SuppressWarnings("serial")
+public class ModelInstantiationException extends Exception {
+		public ModelInstantiationException(String message) {
+			super(message);
+		}
+}
