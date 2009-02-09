9347d8f6b2ebe0b4cf84e77dd08410ca5464cae0
Ivan Poliakov <ivan.poliakov@ncl.ac.uk>
2009-02-09 23:33:17 +0000

    * Removed unused import

--- workcraft/gui/DocumentPlaceholder.java
+++ workcraft/gui/DocumentPlaceholder.java
@@ -2,7 +2,6 @@ package org.workcraft.gui;
 
 import java.awt.Color;
 import java.awt.Graphics;
-import java.awt.Image;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
