d93054d1b2080a7415eb7d4729ddfc1dea90e86a
Ivan Poliakov <ivan.poliakov@ncl.ac.uk>
2009-03-24 15:58:17 +0000

    Added missing files.

---
+++ workcraft/plugins/layout/DotLayoutSettings.java
@@ -0,0 +1,80 @@
+package org.workcraft.plugins.layout;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import org.workcraft.dom.DisplayName;
+import org.workcraft.dom.visual.PropertyChangeListener;
+import org.workcraft.framework.Config;
+import org.workcraft.framework.plugins.Plugin;
+import org.workcraft.gui.propertyeditor.PersistentPropertyEditable;
+import org.workcraft.gui.propertyeditor.PropertyDeclaration;
+import org.workcraft.gui.propertyeditor.PropertyDescriptor;
+
+@DisplayName ("Dot")
+public class DotLayoutSettings implements PersistentPropertyEditable, Plugin {
+	protected static double dotPositionScaleFactor = 0.02;
+	protected static String tmpGraphFilePath = "tmp.dot";
+	protected static String dotCommand = "dot";
+
+	private static LinkedList<PropertyDescriptor> properties;
+
+	public DotLayoutSettings() {
+		properties = new LinkedList<PropertyDescriptor>();
+		properties.add(new PropertyDeclaration("Dot command", "getDotCommand", "setDotCommand", String.class));
+		properties.add(new PropertyDeclaration("Temporary dot file path", "getTmpGraphFilePath", "setTmpGraphFilePath", String.class));
+		properties.add(new PropertyDeclaration("Dot position scale factor", "getDotPositionScaleFactor", "setDotPositionScaleFactor", double.class));
+	}
+	public List<PropertyDescriptor> getPropertyDeclarations() {
+		return properties;
+	}
+
+	public void loadPersistentProperties(Config config) {
+		dotCommand = config.getString("DotLayout.dotCommand", "dot");
+		tmpGraphFilePath = config.getString("DotLayout.tmpGraphFilePath", "tmp.dot");
+		dotPositionScaleFactor = config.getDouble("DotLayout.dotPositionScaleFactor", 0.02);
+	}
+
+	public void storePersistentProperties(Config config) {
+		config.set("DotLayout.dotCommand", dotCommand)	;
+		config.set("DotLayout.tmpGraphFilePath", tmpGraphFilePath);
+		config.set("DotLayout.dotPositionScaleFactor", Double.toString(dotPositionScaleFactor));
+	}
+
+	public static String getTmpGraphFilePath() {
+		return DotLayoutSettings.tmpGraphFilePath;
+	}
+
+	public static void setTmpGraphFilePath(String tmpGraphFilePath) {
+		DotLayoutSettings.tmpGraphFilePath = tmpGraphFilePath;
+	}
+
+	public static String getDotCommand() {
+		return DotLayoutSettings.dotCommand;
+	}
+
+	public static void setDotCommand(String dotCommand) {
+		DotLayoutSettings.dotCommand = dotCommand;
+	}
+
+	public static double getDotPositionScaleFactor() {
+		return DotLayoutSettings.dotPositionScaleFactor;
+	}
+
+	public static void setDotPositionScaleFactor(double dotPositionScaleFactor) {
+		DotLayoutSettings.dotPositionScaleFactor = dotPositionScaleFactor;
+	}
+
+	public void addListener(PropertyChangeListener listener) {
+	}
+
+	public void firePropertyChanged(String propertyName) {
+	}
+
+	public void removeListener(PropertyChangeListener listener) {
+	}
+
+	public String getSection() {
+		return "Layout";
+	}
+}
---
+++ workcraft/plugins/layout/RandomLayoutSettings.java
@@ -0,0 +1,86 @@
+package org.workcraft.plugins.layout;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import org.workcraft.dom.DisplayName;
+import org.workcraft.dom.visual.PropertyChangeListener;
+import org.workcraft.framework.Config;
+import org.workcraft.framework.plugins.Plugin;
+import org.workcraft.gui.propertyeditor.PersistentPropertyEditable;
+import org.workcraft.gui.propertyeditor.PropertyDeclaration;
+import org.workcraft.gui.propertyeditor.PropertyDescriptor;
+
+@DisplayName ("Random")
+public class RandomLayoutSettings implements PersistentPropertyEditable, Plugin {
+	protected static double startX = 0;
+	protected static double startY = 0;
+	protected static double rangeX = 30;
+	protected static double rangeY = 30;
+
+
+	private static LinkedList<PropertyDescriptor> properties;
+
+	public RandomLayoutSettings() {
+		properties = new LinkedList<PropertyDescriptor>();
+		properties.add(new PropertyDeclaration("Start X", "getStartX", "setStartX", double.class));
+		properties.add(new PropertyDeclaration("Start Y", "getStartY", "setStartY", double.class));
+		properties.add(new PropertyDeclaration("Range X", "getRangeX", "setRangeX", double.class));
+		properties.add(new PropertyDeclaration("Range Y", "getRangeY", "setRangeY", double.class));
+
+	}
+	public List<PropertyDescriptor> getPropertyDeclarations() {
+		return properties;
+	}
+
+	public void loadPersistentProperties(Config config) {
+		startX = config.getDouble("RandomLayout.startX", 0);
+		startY = config.getDouble("RandomLayout.startY", 0);
+		rangeX = config.getDouble("RandomLayout.rangeX", 30);
+		rangeY = config.getDouble("RandomLayout.rangeY", 30);
+	}
+
+	public void storePersistentProperties(Config config) {
+		config.setDouble("RandomLayout.startX", startX);
+		config.setDouble("RandomLayout.startY", startY);
+		config.setDouble("RandomLayout.rangeX", rangeX);
+		config.setDouble("RandomLayout.rangeY", rangeY);
+	}
+
+	public void addListener(PropertyChangeListener listener) {
+	}
+
+	public void firePropertyChanged(String propertyName) {
+	}
+
+	public void removeListener(PropertyChangeListener listener) {
+	}
+
+	public String getSection() {
+		return "Layout";
+	}
+	public static double getStartX() {
+		return startX;
+	}
+	public static void setStartX(double startX) {
+		RandomLayoutSettings.startX = startX;
+	}
+	public static double getStartY() {
+		return startY;
+	}
+	public static void setStartY(double startY) {
+		RandomLayoutSettings.startY = startY;
+	}
+	public static double getRangeX() {
+		return rangeX;
+	}
+	public static void setRangeX(double rangeX) {
+		RandomLayoutSettings.rangeX = rangeX;
+	}
+	public static double getRangeY() {
+		return rangeY;
+	}
+	public static void setRangeY(double rangeY) {
+		RandomLayoutSettings.rangeY = rangeY;
+	}
+}
