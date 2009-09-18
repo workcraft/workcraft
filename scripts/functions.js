a408f81b2669715b7f047f1ae5cafbe7473b54c5
Ivan Poliakov <ivan.poliakov@ncl.ac.uk>
2009-09-18 19:04:26 +0100

    t-------------- This line and the following will be ignored --------------
    added:
      build/
      build/build.xml
      config/
      config/config.xml
      config/plugins.xml
      config/uilayout.xml
      images/
      images/logo.png
      lib/
      lib/batik-awt-util.jar
      lib/batik-svggen.jar
      lib/batik-util.jar
      lib/commons-logging-1.1.jar
      lib/flexdock-0.5.1.jar
      lib/jedit.jar
      lib/jga-0.8-lgpl.jar
      lib/js.jar
      lib/junit-4.5.jar
      lib/substance.jar
      org/
      scripts/
      scripts/functions.js
      scripts/shutdown.js
      scripts/startup.js
    renamed:
      workcraft/ => org/workcraft/
    unknown:
      .bzrignore
      .classpath
      .externalToolBuilders/
      .project
      .settings/

---
+++ scripts/functions.js
@@ -0,0 +1,66 @@
+// standard function shortcuts
+function quit() {
+	framework.shutdown();
+}
+
+function done() {
+	framework.shutdown();
+
+}
+
+function exit() {
+	framework.shutdown();
+}	
+
+function shutdown() {
+	framework.shutdown();
+}
+
+function load(x) {
+	return framework.load(x);
+}
+
+function save(x) {
+	framework.save(x);
+
+}
+
+function print(x) {
+	java.lang.System.out.print(x);
+}
+
+function println(x) {
+	java.lang.System.out.println(x);
+}
+
+function printerr(x) {
+	java.lang.System.err.print(x);
+}
+
+function printlnerr(x) {
+	java.lang.System.err.println(x);
+}
+
+function setcvar(k,v) {
+	framework.setConfigVar(k,v);
+}
+
+function getcvar(k) {
+	return framework.getConfigVar(k);
+}
+
+function saveconfig() {
+	framework.saveConfig("config/config.xml");
+}
+
+function loadconfig() {
+	framework.loadConfig("config/config.xml");
+}
+
+function startgui() {
+	framework.startGUI();
+}
+
+function shutdowngui() {
+	framework.shutdownGUI();
+}
---
+++ scripts/startup.js
@@ -0,0 +1,3 @@
+// startup actions
+
+loadconfig();
