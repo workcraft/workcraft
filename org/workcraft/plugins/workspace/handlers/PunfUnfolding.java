f94542e08c917e9a38d7513f84638d022b84511b
Ivan Poliakov <ivan.poliakov@ncl.ac.uk>
2010-02-11 12:39:21 +0000

    Added "unfold" command to workspace context menu for .g files.

---
+++ org/workcraft/plugins/workspace/handlers/PunfUnfolding.java
@@ -0,0 +1,71 @@
+package org.workcraft.plugins.workspace.handlers;
+
+import java.io.File;
+
+import javax.swing.JFileChooser;
+import javax.swing.JOptionPane;
+
+import org.workcraft.Config;
+import org.workcraft.ConfigurablePlugin;
+import org.workcraft.Framework;
+import org.workcraft.annotations.DisplayName;
+import org.workcraft.gui.FileFilters;
+import org.workcraft.gui.tasks.TaskFailureNotifier;
+import org.workcraft.plugins.verification.tasks.PunfTask;
+import org.workcraft.workspace.FileHandler;
+
+@DisplayName("Unfold using punf")
+public class PunfUnfolding implements FileHandler, ConfigurablePlugin {
+	private String lastUnfoldingPath = null;
+
+	public boolean accept(File f) {
+		if (f.getName().endsWith(".g"))
+			return true;
+		else
+			return false;
+	}
+
+	public void execute(File f, Framework framework) {
+		JFileChooser fc = new JFileChooser();
+		fc.setDialogType(JFileChooser.SAVE_DIALOG);
+
+		if (lastUnfoldingPath != null)
+			fc.setCurrentDirectory(new File(lastUnfoldingPath));
+
+		fc.setFileFilter(new FileFilters.GenericFileFilter(".mci", "Petri Net Unfolding Prefix (.mci)"));
+		fc.setMultiSelectionEnabled(false);
+		fc.setDialogTitle("Save unfolding as...");
+
+		String path;
+
+		while (true) {
+			if(fc.showSaveDialog(framework.getMainWindow())==JFileChooser.APPROVE_OPTION) {
+				path = fc.getSelectedFile().getPath();
+
+				if (!path.endsWith(".mci"))
+					path += ".mci";
+
+				File out = new File(path);
+
+				if (!out.exists())
+					break;
+				else
+					if (JOptionPane.showConfirmDialog(framework.getMainWindow(), "The file \"" + out.getName()+"\" already exists. Do you want to overwrite it?", "Confirm",
+							JOptionPane.YES_NO_OPTION)==JOptionPane.YES_OPTION)
+						break;
+			} else
+				return;
+		}
+
+		framework.getConfig().set("Verification.punf.lastUnfoldingPath", path);
+
+		PunfTask task = new PunfTask(f.getPath(), path);
+
+		framework.getTaskManager().queue(task, "Unfolding " + f.getName(), new TaskFailureNotifier());
+	}
+
+	@Override
+	public void readConfig(Config config) {
+		lastUnfoldingPath = config.getString("Verification.punf.lastUnfoldingPath", null);
+	}
+}
