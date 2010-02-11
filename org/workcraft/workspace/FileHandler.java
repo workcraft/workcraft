c926072bfccd471fcbbb4f30b208b11bacaa526e
Ivan Poliakov <ivan.poliakov@ncl.ac.uk>
2010-02-11 12:41:50 +0000

    Added framework parameter to execute method.

--- org/workcraft/workspace/FileHandler.java
+++ org/workcraft/workspace/FileHandler.java
@@ -23,9 +23,10 @@ package org.workcraft.workspace;
 
 import java.io.File;
 
+import org.workcraft.Framework;
 import org.workcraft.Plugin;
 
 public interface FileHandler extends Plugin {
 	public boolean accept (File f);
-	public void execute (File f);
+	public void execute (File f, Framework framework);
 }
