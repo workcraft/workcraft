e9267a4ebe5e198e2ba5efb76174b6f62d3e10ff
Ivan Poliakov <ivan.poliakov@ncl.ac.uk>
2010-02-14 17:59:31 +0000

    Fixed issues with templates

--- org/workcraft/gui/tasks/TaskFailureNotifier.java
+++ org/workcraft/gui/tasks/TaskFailureNotifier.java
@@ -4,33 +4,31 @@ import javax.swing.JOptionPane;
 import javax.swing.SwingUtilities;
 
 import org.workcraft.tasks.DummyProgressMonitor;
-import org.workcraft.tasks.ExceptionResult;
-import org.workcraft.tasks.ExternalProcessResult;
 import org.workcraft.tasks.Result;
-import org.workcraft.tasks.Result.ExitStatus;
+import org.workcraft.tasks.Result.Outcome;
 
-public class TaskFailureNotifier extends DummyProgressMonitor {
+public class TaskFailureNotifier extends DummyProgressMonitor<Object> {
 	String errorMessage = "";
 
-
 	@Override
-	public void finished(final Result result, final String description) {
-		if (result.getExitStatus() == ExitStatus.FAILED) {
+	public void finished(final Result<? extends Object> result, final String description) {
+		if (result.getOutcome() == Outcome.FAILED) {
 			SwingUtilities.invokeLater(new Runnable() {
 				@Override
 				public void run() {
 					String message = "Unfortunately, the task \"" + description + "\" has failed to complete as expected.";
-					if (result instanceof ExceptionResult) {
-						Throwable reason = ((ExceptionResult)result).getReason();
+
+					if (result.getCause() != null) {
+						Throwable reason = result.getCause();
 						reason.printStackTrace();
 						message += "\n\nThe reason was: " + reason.toString();
 						message += "\nPlease see the \"Problems\" tab for further details.";
-					} else if (result instanceof ExternalProcessResult){
-						ExternalProcessResult epr = ((ExternalProcessResult)result);
-						message += "\n\nExternal process has returned code " + epr.getReturnCode();
-						if (!errorMessage.isEmpty())
-							message += " and reported the following errors:\n" + errorMessage;
 					}
+
+					if (!errorMessage.isEmpty()) {
+						message += "\n\nFollowing errors were reported:\n" + errorMessage;
+					}
+
 					JOptionPane.showMessageDialog(null, message, "Task failed", JOptionPane.ERROR_MESSAGE);
 				}
 			});
@@ -38,7 +36,7 @@ public class TaskFailureNotifier extends DummyProgressMonitor {
 	}
 
 	@Override
-	public void logErrorMessage(String message) {
-		errorMessage = errorMessage + message;
+	public void stderr(byte[] data) {
+		errorMessage = errorMessage + new String(data);
 	}
 }
