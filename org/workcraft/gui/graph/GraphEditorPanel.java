ccd4793215e78f1fa00a11596802d45a38cc4424
Ivan Poliakov <ivan.poliakov@ncl.ac.uk>
2010-05-19 17:17:10 +0100

    Fixed slowdown caused by Workspace window being redrawn too often

--- org/workcraft/gui/graph/GraphEditorPanel.java
+++ org/workcraft/gui/graph/GraphEditorPanel.java
@@ -70,8 +70,8 @@ public class GraphEditorPanel extends JPanel implements StateObserver, GraphEdit
 			if (e instanceof PropertyChangedEvent || e instanceof TransformChangedEvent) {
 				repaint();
 				mainWindow.getPropertyView().repaint();
-
-				workspaceEntry.setChanged(true);
+				if (!workspaceEntry.isChanged())
+					workspaceEntry.setChanged(true);
 			}
 		}
 	}
@@ -181,14 +181,14 @@ public class GraphEditorPanel extends JPanel implements StateObserver, GraphEdit
 		}
 
 		g2d.transform(view.getTransform());
+
 		g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-		//g2d.setRenderingHint(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_SPEED);
+		g2d.setRenderingHint(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_SPEED);
 		g2d.setRenderingHint(RenderingHints.KEY_FRACTIONALMETRICS, RenderingHints.VALUE_FRACTIONALMETRICS_ON);
-
-		g2d.setRenderingHint(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
 		g2d.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING, RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
-		g2d.setRenderingHint(RenderingHints.KEY_STROKE_CONTROL, RenderingHints.VALUE_STROKE_PURE);
+//		g2d.setRenderingHint(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
 
+		g2d.setRenderingHint(RenderingHints.KEY_STROKE_CONTROL, RenderingHints.VALUE_STROKE_PURE);
 
 		visualModel.draw(g2d);
 
