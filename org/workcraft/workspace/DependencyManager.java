91df729fd1ec35aee5ead2383263e6d01568f404
Ivan Poliakov <ivan.poliakov@ncl.ac.uk>
2010-05-25 12:41:53 +0100

    Started implementing proper MPSat result handling

---
+++ org/workcraft/workspace/DependencyManager.java
@@ -0,0 +1,18 @@
+package org.workcraft.workspace;
+
+import java.util.List;
+
+import org.workcraft.gui.workspace.Path;
+import org.workcraft.util.ListMap;
+
+public class DependencyManager {
+	private ListMap<Path<String>, Path<String>> associations;
+
+	public void createAssociation(Path<String> dependentFile, Path<String> masterFile) {
+		associations.put(masterFile, dependentFile);
+	}
+
+	public List<Path<String>> getAssociatedFiles(Path<String> masterFile) {
+		return associations.get(masterFile);
+	}
+}
