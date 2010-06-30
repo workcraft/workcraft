2161170712429a014ba7aabbfcf9bca4b5e12ae4
Arseniy Alekseyev <rotsor@gmail.com>
2010-06-30 12:24:24 +0100

    Implemented initial version of parsing of DOT's splines.

--- org/workcraft/plugins/layout/DotLayout.java
+++ org/workcraft/plugins/layout/DotLayout.java
@@ -21,10 +21,14 @@
 
 package org.workcraft.plugins.layout;
 
+import java.awt.geom.Point2D;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -33,11 +37,16 @@ import org.workcraft.PluginConsumer;
 import org.workcraft.PluginProvider;
 import org.workcraft.Tool;
 import org.workcraft.annotations.DisplayName;
+import org.workcraft.dom.Connection;
 import org.workcraft.dom.Model;
 import org.workcraft.dom.Node;
 import org.workcraft.dom.visual.Movable;
 import org.workcraft.dom.visual.MovableHelper;
 import org.workcraft.dom.visual.VisualModel;
+import org.workcraft.dom.visual.connections.ControlPoint;
+import org.workcraft.dom.visual.connections.Polyline;
+import org.workcraft.dom.visual.connections.VisualConnection;
+import org.workcraft.dom.visual.connections.VisualConnection.ConnectionType;
 import org.workcraft.exceptions.LayoutException;
 import org.workcraft.exceptions.ModelValidationException;
 import org.workcraft.exceptions.SerialisationException;
@@ -45,6 +54,7 @@ import org.workcraft.interop.Exporter;
 import org.workcraft.interop.SynchronousExternalProcess;
 import org.workcraft.serialisation.Format;
 import org.workcraft.util.Export;
+import org.workcraft.util.FileUtils;
 
 @DisplayName ("Layout using dot")
 public class DotLayout implements Tool, PluginConsumer {
@@ -60,15 +70,54 @@ public class DotLayout implements Tool, PluginConsumer {
 		out.close();
 	}
 
-	private static String fileToString(File file) throws IOException {
-		FileInputStream f = new FileInputStream(file);
-		byte[] buf = new byte[f.available()];
-		f.read(buf);
-		return new String(buf);
+	List<Point2D> parseConnectionSpline(String pos)
+	{
+		ArrayList<Point2D> result = new ArrayList<Point2D>();
+		Point2D end = null;
+		String [] split = pos.split(" ");
+
+		for(String s : split)
+		{
+			String [] ss = s.split(",");
+			if(ss.length <2 || ss.length>3)
+				throw new RuntimeException("bad connection position format");
+			double pointsToInches = 1.0/72;
+			if(ss.length == 3)
+			{
+				double x = Double.parseDouble(ss[1])*pointsToInches ;
+				double y = Double.parseDouble(ss[2])*pointsToInches;
+				Point2D p = new Point2D.Double(x,y);
+				if(ss[0].equals("s"))
+					result.add(0,p);
+				else
+					if(ss[0].equals("e"))
+						end = p;
+					else
+						throw new RuntimeException("bad connection position format");
+			}
+			else
+			{
+				double x = Double.parseDouble(ss[0])*pointsToInches;
+				double y = Double.parseDouble(ss[1])*pointsToInches;
+				result.add(0,new Point2D.Double(x,y));
+			}
+		}
+
+		if(end!=null)
+			result.add(0,end);
+		return result;
 	}
 
 	private void applyLayout(String in, VisualModel model) {
-		Pattern regexp = Pattern.compile("\\s*\\\"?(.+)\\\"?\\s+\\[width=\\\"?(-?\\d*\\.?\\d+)\\\"?\\s*,\\s*height=\\\"?(-?\\d*\\.?\\d+)\\\"?\\s*,\\s*pos=\\\"?(-?\\d+)\\s*,\\s*(-?\\d+)\\\"?\\];\\s*\\n");
+
+		System.out.println("=================================");
+		System.out.println("dot output: ");
+		System.out.println("=================================");
+		System.out.println(in);
+
+		System.out.println("=================================");
+		Pattern regexp = Pattern.compile("\\s*\\\"?(.+)\\\"?\\s+\\[width=\\\"?(-?\\d*\\.?\\d+)\\\"?\\s*,\\s*height=\\\"?(-?\\d*\\.?\\d+)\\\"?\\s*, fixedsize=true,\\s*pos=\\\"?(-?\\d*\\.?\\d+)\\s*,\\s*(-?\\d*\\.?\\d+)\\\"?\\];\\s*\\n");
+		Pattern regexpConn = Pattern.compile("\\s*\\\"?(.+)\\\"?\\s+\\-\\>\\s+\\\"?(.+)\\\"?\\s+\\[pos=\\\"?([se,0-9\\ ]+)\\\"\\];\\n");
 		Matcher matcher = regexp.matcher(in);
 
 		while(matcher.find()) {
@@ -81,9 +130,50 @@ public class DotLayout implements Tool, PluginConsumer {
 			Movable m = (Movable)comp;
 			MovableHelper.resetTransform(m);
 			MovableHelper.translate(m,
-					Integer.parseInt(matcher.group(4))*DotLayoutSettings.dotPositionScaleFactor,
-					-Integer.parseInt(matcher.group(5))*DotLayoutSettings.dotPositionScaleFactor);
+					Double.parseDouble(matcher.group(4))*1.0/72,
+					Double.parseDouble(matcher.group(5))*1.0/72);
 		}
+
+		matcher = regexpConn.matcher(in);
+
+		while(matcher.find())
+		{
+			String from = matcher.group(1);
+			String to = matcher.group(2);
+			String info = matcher.group(3);
+
+			System.out.println("connection info found: " + info);
+
+			Node comp1 = model.getNodeByReference(from);
+			Node comp2 = model.getNodeByReference(to);
+			Set<Connection> connections = model.getConnections(comp1);
+			Connection con = null;
+			for(Connection c : connections)
+			{
+				if(c.getSecond() == comp2)
+					con = c;
+			}
+			if(con!=null)
+			{
+				System.out.println("Applying!");
+
+				VisualConnection vc = (VisualConnection)con;
+				vc.setConnectionType(ConnectionType.POLYLINE);
+
+				Polyline poly = (Polyline)vc.getGraphic();
+				poly.remove(poly.getChildren());
+				List<Point2D> points = parseConnectionSpline(info);
+
+				for(int i=points.size()-1;i>=0;i--)
+				{
+					Point2D p = points.get(i);
+					ControlPoint cp = new ControlPoint();
+					cp.setPosition(p);
+					poly.add(cp);
+				}
+			}
+		}
+
 	}
 
 	private void cleanUp() {
@@ -107,7 +197,7 @@ public class DotLayout implements Tool, PluginConsumer {
 					new String[] {DotLayoutSettings.dotCommand, "-Tdot", "-o", tmp2.getAbsolutePath(), tmp1.getAbsolutePath()}, ".");
 			p.start(10000);
 			if(p.getReturnCode()==0) {
-				String in = fileToString(tmp2);
+				String in = FileUtils.readAllText(tmp2);
 				applyLayout(in, (VisualModel)model);
 			}
 			else
@@ -119,7 +209,7 @@ public class DotLayout implements Tool, PluginConsumer {
 		} catch (SerialisationException e) {
 			throw new RuntimeException(e);
 		} finally {
-			cleanUp();
+		//	cleanUp();
 		}
 	}
 
