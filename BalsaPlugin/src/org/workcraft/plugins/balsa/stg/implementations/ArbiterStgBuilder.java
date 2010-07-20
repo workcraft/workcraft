af64ce40e652be2bc92a744de703e1f573c3cd8f
Arseniy Alekseyev <rotsor@gmail.com>
2010-07-20 14:41:07 +0100

    Implemented a few STGs

--- BalsaPlugin/src/org/workcraft/plugins/balsa/stg/implementations/ArbiterStgBuilder.java
+++ BalsaPlugin/src/org/workcraft/plugins/balsa/stg/implementations/ArbiterStgBuilder.java
@@ -1,6 +1,7 @@
 package org.workcraft.plugins.balsa.stg.implementations;
 
 import org.workcraft.plugins.balsa.stg.generated.*;
+import org.workcraft.plugins.balsa.stgbuilder.StgPlace;
 import org.workcraft.plugins.balsa.stgbuilder.StrictPetriBuilder;
 
 public final class ArbiterStgBuilder extends ArbiterStgBuilderBase {
@@ -8,6 +9,14 @@ public final class ArbiterStgBuilder extends ArbiterStgBuilderBase {
 	@Override
 	public void buildStg(Arbiter component, ArbiterHandshakes h,
 			StrictPetriBuilder b) {
-		throw new org.workcraft.exceptions.NotImplementedException();
+		StgPlace place = b.buildPlace(1);
+		b.connect(h.inpA.go(), h.outA.go());
+		b.connect(h.inpB.go(), h.outB.go());
+		b.connect(place, h.outA.go());
+		b.connect(place, h.outB.go());
+		b.connect(h.outA.done(), place);
+		b.connect(h.outB.done(), place);
+		b.connect(h.outA.done(), h.inpA.done());
+		b.connect(h.outB.done(), h.inpB.done());
 	}
 }
