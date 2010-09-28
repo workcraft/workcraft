b2d2a685d7d137a92cc1567a402700d4e4f3b313
Arseniy Alekseyev <rotsor@gmail.com>
2010-09-28 16:23:41 +0100

    Improved main window loading time by moving the SVG icon load to a separate thread.

--- Workcraft/src/org/workcraft/gui/MainWindowIconManager.java
+++ Workcraft/src/org/workcraft/gui/MainWindowIconManager.java
@@ -4,24 +4,46 @@ import java.awt.Color;
 import java.awt.Image;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.lang.reflect.InvocationTargetException;
+
+import javax.swing.SwingUtilities;
 
 import org.workcraft.util.GUI;
 
 public class MainWindowIconManager {
 	static void apply(final MainWindow window)
 	{
-		final Image active = GUI.createIconFromSVG("images/icons/svg/place.svg", 32, 32, Color.WHITE).getImage();
-		final Image inactive = GUI.createIconFromSVG("images/icons/svg/place_empty.svg", 32, 32, Color.WHITE).getImage();
-		window.setIconImage(active);
-		window.addWindowListener(new WindowAdapter() {
+		new Thread(new Runnable()
+		{
 			@Override
-			public void windowDeactivated(WindowEvent e) {
-				window.setIconImage(inactive);
+			public void run() {
+				final Image active = GUI.createIconFromSVG("images/icons/svg/place.svg", 32, 32, Color.WHITE).getImage();
+				final Image inactive = GUI.createIconFromSVG("images/icons/svg/place_empty.svg", 32, 32, Color.WHITE).getImage();
+
+				try {
+					SwingUtilities.invokeAndWait(new Runnable(){
+						@Override
+						public void run() {
+							window.setIconImage(window.isActive()?active:inactive);
+							window.addWindowListener(new WindowAdapter() {
+								@Override
+								public void windowDeactivated(WindowEvent e) {
+									window.setIconImage(inactive);
+								}
+								@Override
+								public void windowActivated(WindowEvent e) {
+									window.setIconImage(active);
+								}
+							});
+						}
+					});
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				} catch (InvocationTargetException e) {
+					e.printStackTrace();
+				}
 			}
-			@Override
-			public void windowActivated(WindowEvent e) {
-				window.setIconImage(active);
-			}
-		});
+		}
+		).start();
 	}
 }
