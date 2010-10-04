1920c0de6039f4bad1112ef8e4dce6a259f13aa5
Stanislav Golubtsov <sgaflv@gmail.com>
2010-10-04 21:45:22 +0100

    - STG reachability analysis

---
+++ Workcraft/src/org/workcraft/plugins/shared/MpsatStgReachabilityResultHandler.java
@@ -0,0 +1,46 @@
+package org.workcraft.plugins.shared;
+
+import java.util.List;
+
+import javax.swing.JOptionPane;
+
+import org.workcraft.Trace;
+import org.workcraft.plugins.shared.tasks.MpsatChainResult;
+import org.workcraft.plugins.shared.tasks.MpsatChainTask;
+import org.workcraft.plugins.verification.gui.SolutionsDialog;
+import org.workcraft.tasks.Result;
+import org.workcraft.util.GUI;
+
+
+final class MpsatStgReachabilityResultHandler implements Runnable {
+
+	private final Result<? extends MpsatChainResult> mpsatChainResult;
+	private final MpsatChainTask task;
+
+	MpsatStgReachabilityResultHandler(
+			MpsatChainTask task,
+			Result<? extends MpsatChainResult> mpsatChainResult) {
+		this.task = task;
+		this.mpsatChainResult = mpsatChainResult;
+	}
+
+
+	@Override
+	public void run() {
+		MpsatResultParser mdp = new MpsatResultParser(mpsatChainResult.getReturnValue().getMpsatResult().getReturnValue());
+
+		List<Trace> solutions = mdp.getSolutions();
+
+		if (!solutions.isEmpty()) {
+			String message = "The system has a non-persistent output.\n";
+
+			final SolutionsDialog solutionsDialog = new SolutionsDialog(task, message, solutions);
+
+			GUI.centerAndSizeToParent(solutionsDialog, task.getFramework().getMainWindow());
+
+			solutionsDialog.setVisible(true);
+		} else
+			JOptionPane.showMessageDialog(null, "All system outputs are persistent.");
+	}
+
+}
