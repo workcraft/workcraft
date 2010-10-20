e783e5c8ef42e26553bd4d8e456ba73c27de4a9d
Arseniy Alekseyev <rotsor@gmail.com>
2010-10-20 12:29:18 +0100

    added forgotten files

---
+++ Workcraft/src/org/workcraft/gui/graph/tools/Decorator.java
@@ -0,0 +1,26 @@
+package org.workcraft.gui.graph.tools;
+
+import org.workcraft.dom.Node;
+
+public interface Decorator {
+	/**
+	 * Returns decoration to apply to this node and its children.
+	 * Overrides decoration applied by parents unless the returned decoration is null.
+	 * @param node
+	 * The node to be decorated
+	 * @return
+	 * Decoration to be applied
+	 */
+	Decoration getDecoration(Node node);
+
+	public class Empty implements Decorator {
+
+		public static Empty INSTANCE = new Empty();
+
+		@Override
+		public Decoration getDecoration(Node node) {
+			return null;
+		}
+
+	}
+}
