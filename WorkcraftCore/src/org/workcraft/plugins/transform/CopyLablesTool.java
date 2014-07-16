bbab3b7255db79f7ec213cb43702bd36c2217160
Danil Sokolov <danilovesky@gmail.com>
2014-07-16 14:18:04 +0100

    Corrections for setting component label from their unique names to respect the groups and pages
    Fixes #70.

--- WorkcraftCore/src/org/workcraft/plugins/transform/CopyLablesTool.java
+++ WorkcraftCore/src/org/workcraft/plugins/transform/CopyLablesTool.java
@@ -26,6 +26,7 @@ import org.workcraft.dom.Node;
 import org.workcraft.dom.math.MathModel;
 import org.workcraft.dom.visual.VisualComponent;
 import org.workcraft.dom.visual.VisualModel;
+import org.workcraft.util.Hierarchy;
 import org.workcraft.util.WorkspaceUtils;
 import org.workcraft.workspace.WorkspaceEntry;
 
@@ -46,12 +47,9 @@ public class CopyLablesTool implements Tool {
 		VisualModel visualModel = WorkspaceUtils.getAs(we, VisualModel.class);
 		if (visualModel != null) {
 			MathModel mathModel = (MathModel)visualModel.getMathModel();
-			for (Node node : visualModel.getRoot().getChildren()) {
-				if (node instanceof VisualComponent) {
-					VisualComponent visualComponent = (VisualComponent)node;
-					Node refComponent = ((VisualComponent)node).getReferencedComponent();
-					visualComponent.setLabel(mathModel.getNodeReference(refComponent));
-				}
+			for (VisualComponent visualComponent : Hierarchy.getDescendantsOfType(visualModel.getRoot(), VisualComponent.class)) {
+				Node refComponent = visualComponent.getReferencedComponent();
+				visualComponent.setLabel(mathModel.getName(refComponent));
 			}
 		}
 	}
