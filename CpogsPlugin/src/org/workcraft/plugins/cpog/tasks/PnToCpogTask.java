e19a23a95455b68061651df2adc96e43a51ff96c
Alessandro de Gennaro <alex10.dege@gmail.com>
2015-09-24 16:20:55 +0100

    Converter from Petri nets to Conditional partial order graphs implemented. The external library jbpt has been added inside the ThirdParty folder.

---
+++ CpogsPlugin/src/org/workcraft/plugins/cpog/tasks/PnToCpogTask.java
@@ -0,0 +1,67 @@
+package org.workcraft.plugins.cpog.tasks;
+
+import org.workcraft.Framework;
+import org.workcraft.gui.workspace.Path;
+import org.workcraft.plugins.cpog.CpogDescriptor;
+import org.workcraft.plugins.cpog.VisualCPOG;
+import org.workcraft.plugins.cpog.tools.PnToCpogConverter;
+import org.workcraft.plugins.petri.VisualPetriNet;
+import org.workcraft.plugins.shared.CommonEditorSettings;
+import org.workcraft.tasks.ProgressMonitor;
+import org.workcraft.tasks.Result;
+import org.workcraft.tasks.Result.Outcome;
+import org.workcraft.tasks.Task;
+import org.workcraft.workspace.ModelEntry;
+import org.workcraft.workspace.Workspace;
+import org.workcraft.workspace.WorkspaceEntry;
+
+public class PnToCpogTask implements Task<PnToCpogResult>{
+
+	private final WorkspaceEntry we;
+
+	// conversion-related variables
+	private VisualPetriNet pn;
+	private VisualCPOG cpog;
+
+	public PnToCpogTask(WorkspaceEntry we) {
+		this.we = we;
+	}
+
+
+	@Override
+	public Result<? extends PnToCpogResult> run(
+			ProgressMonitor<? super PnToCpogResult> monitor) {
+
+		PnToCpogResult result;
+		Outcome outcome;
+
+		// reading Petri net from workspace
+		pn = (VisualPetriNet)(we.getModelEntry().getVisualModel());
+
+		// instantiating converter
+		PnToCpogConverter converter = new PnToCpogConverter(pn);
+
+		// get the partial orders from the Petri net introduced
+		cpog = converter.run();
+
+		// checking that conversion process terminated correctly
+		if (cpog == null){
+			result = new PnToCpogResult("Conversion failed.", null);
+			outcome = Outcome.FAILED;
+		}else{
+			result = new PnToCpogResult("Conversion terminated correctly.", cpog);
+			outcome = Outcome.FINISHED;
+
+			final Framework framework = Framework.getInstance();
+			final Workspace workspace = framework.getWorkspace();
+			final Path<String> directory = we.getWorkspacePath().getParent();
+			final ModelEntry me = new ModelEntry(new CpogDescriptor(), cpog);
+			boolean openInEditor = (me.isVisual() || CommonEditorSettings.getOpenNonvisual());
+			workspace.add(directory, we.getTitle().concat("_cpog"), me, false, openInEditor);
+		}
+
+		return new Result<PnToCpogResult>(outcome, result);
+
+	}
+
+}
--- CpogsPlugin/src/org/workcraft/plugins/cpog/tools/PnToCpogTool.java
+++ CpogsPlugin/src/org/workcraft/plugins/cpog/tools/PnToCpogTool.java
@@ -1,14 +1,14 @@
-package org.workcraft.plugins.fst.tools;
+package org.workcraft.plugins.cpog.tools;
 
 import org.workcraft.Framework;
 import org.workcraft.Tool;
-import org.workcraft.plugins.fst.task.PnToFsmConversionResultHandler;
-import org.workcraft.plugins.fst.task.WriteSgConversionTask;
+import org.workcraft.plugins.cpog.tasks.PnToCpogHandler;
+import org.workcraft.plugins.cpog.tasks.PnToCpogTask;
 import org.workcraft.plugins.petri.PetriNet;
 import org.workcraft.util.WorkspaceUtils;
 import org.workcraft.workspace.WorkspaceEntry;
 
-public class PnToFsmConverterTool implements Tool {
+public class PnToCpogTool implements Tool {
 
 	@Override
 	public boolean isApplicableTo(WorkspaceEntry we) {
@@ -22,14 +22,18 @@ public class PnToFsmConverterTool implements Tool {
 
 	@Override
 	public String getDisplayName() {
-		return "Finite State Machine [Petrify]";
+		return "Conditional Partial Order Graph";
 	}
 
 	@Override
 	public void run(WorkspaceEntry we) {
-		WriteSgConversionTask task = new WriteSgConversionTask(we);
 		final Framework framework = Framework.getInstance();
-		framework.getTaskManager().queue(task, "Building state graph", new PnToFsmConversionResultHandler(task));
-	}
 
+		// Instantiate Solver
+		PnToCpogTask task = new PnToCpogTask(we);
+		// Instantiate object for handling solution
+		PnToCpogHandler result = new PnToCpogHandler(task);
+		//Run both
+		framework.getTaskManager().queue(task, "Converting Petri net into CPOG...", result);
+	}
 }
