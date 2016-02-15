ed724e5ac14b961f63c644a17adf7227fbfa3019
Daniel Martí <mvdan@mvdan.cc>
2016-02-15 14:25:45 +0000

    Remove redundant src_dir vars

--- build_distr_linux.sh
+++ build_distr_linux.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
 
-src_dir="."
 template_dir="../../distr-template-linux"
 
-./build_distr.sh -s $src_dir -t $template_dir
+./build_distr.sh -t $template_dir
 rm -f  ${distr_dir}/workcraft.bat
 tar czf "${distr_dir}.tar.gz" $distr_dir
 rm -rf $distr_dir
--- build_distr_windows.sh
+++ build_distr_windows.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
 
-src_dir="."
 template_dir="../../distr-template-windows"
 
-./build_distr.sh -s $src_dir -t $template_dir
+./build_distr.sh -t $template_dir
 rm -f  ${distr_dir}/workcraft
 7z a -r "${distr_dir}.zip" $distr_dir
 rm -rf $distr_dir
