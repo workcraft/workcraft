d4f9c4fb6a19a33babf8614259b3915766d17550
Daniel Martí <mvdan@mvdan.cc>
2016-02-15 15:54:48 +0000

    Name dist scripts more aptly
    This also helps differentiate them with build.gradle.

--- dist_linux.sh
+++ dist_linux.sh
@@ -3,6 +3,6 @@
 template_dir="dist-template/linux"
 dist_dir="dist_linux"
 
-./build_distr.sh -t $template_dir -d $dist_dir
+./dist.sh -t $template_dir -d $dist_dir
 rm -f  ${dist_dir}/workcraft.bat
 tar czf "${dist_dir}.tar.gz" $dist_dir
--- dist_windows.sh
+++ dist_windows.sh
@@ -3,6 +3,6 @@
 template_dir="dist-template/windows"
 dist_dir="dist_windows"
 
-./build_distr.sh -t $template_dir -d $dist_dir
+./dist.sh -t $template_dir -d $dist_dir
 rm -f  ${dist_dir}/workcraft
 7z a -r "${dist_dir}.zip" $dist_dir
