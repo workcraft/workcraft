ea8a26907b570d326c40079bb252fde9164e6b1b
Daniel Martí <mvdan@mvdan.cc>
2016-02-15 15:31:30 +0000

    Add template sudmobule, start producing a dist

--- build_distr.sh
+++ build_distr.sh
@@ -19,17 +19,17 @@ tool_plugin_dirs="
     PunfPlugin
     PetrifyExtraPlugin
     PetrifyPlugin"
-core_dirs="WorkcraftCore"
+core_dir="WorkcraftCore"
 core_files="LICENSE README.md workcraft workcraft.bat"
 
-distr_dir=dist
-template_dir="../../distr-template"
+dist_dir=dist
+template_dir="dist-template/linux"
 
 description_msg="`basename $0`: creates a distribution for Workcraft"
-usage_msg="Usage: `basename $0` [-d DISTR_DIR] [-t TEMPLATE_DIR] [-h | --help]"
+usage_msg="Usage: `basename $0` [-d DIST_DIR] [-t TEMPLATE_DIR] [-h | --help]"
 params_msg="
-  -d DISTR_DIR : distribution directory (default: $distr_dir)
-  -t TEMPLATE_DIRECTORY: template directory (default: $template_dir))
+  -d DIST_DIR : distribution directory (default: $dist_dir)
+  -t TEMPLATE_DIR: template directory (default: $template_dir))
   -h, --help : print this help"
 help_msg="${description_msg}\n\n${usage_msg}\n${params_msg}\n"
 
@@ -39,40 +39,40 @@ err() {
 }
 
 # Process parameters
-for param in $*
-do
+for param in $*; do
     case $param in
-        -d) distr_dir=$2; shift 2;;
+        -d) dist_dir=$2; shift 2;;
         -t) template_dir=$2; shift 2;;
         -h | --help) printf "$help_msg"; exit 0;
     esac
 done
 
 # Create distr directory
-if [[ -e $distr_dir ]]
-then
-    err "Distribution directory already exists: $distr_dir"
+if [[ -e $dist_dir ]]; then
+    err "Distribution directory already exists: $dist_dir"
 fi
-mkdir $distr_dir
+
+mkdir -p $dist_dir
 
 # Copy the template content
 if [[ ! -d $template_dir ]]; then
     err "Template directory not found: $template_dir"
 fi
-cp -r $template_dir/* $distr_dir/
 
-# Copy core and plugin classes, third-party libraries, documentation and misc files
-for i in $core_dirs $tool_plugin_dirs $model_plugin_dirs
-do
-    mkdir $distr_dir/$i
-    cp -r $i/bin $distr_dir/$i/
+cp -r $template_dir/* $dist_dir/
+
+cp $core_dir/build/libs/*.jar $dist_dir/workcraft.jar
+
+mkdir -p $dist_dir/plugins
+
+for d in $tool_plugin_dirs $model_plugin_dirs; do
+    cp $d/build/libs/*.jar $dist_dir/plugins/
 done
 
 for d in doc/*; do
-    cp -r $d $distr_dir/
+    cp -r $d $dist_dir/
 done
 
-for i in $core_files
-do
-    cp -r $i $distr_dir/
+for f in $core_files; do
+    cp $f $dist_dir/
 done
