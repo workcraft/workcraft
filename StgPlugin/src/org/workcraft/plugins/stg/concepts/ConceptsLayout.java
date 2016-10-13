2a87910176fb4a3cf47592a7f77929018d904c78
Jonny Beaumont <jonathan.r.beaumont@gmail.com>
2016-10-13 15:00:31 +0100

    Concepts layout now uses proxy places with read-arcs

--- StgPlugin/src/org/workcraft/plugins/stg/concepts/ConceptsLayout.java
+++ StgPlugin/src/org/workcraft/plugins/stg/concepts/ConceptsLayout.java
@@ -1,11 +1,21 @@
 package org.workcraft.plugins.stg.concepts;
 import java.awt.geom.Point2D;
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.HashSet;
 
+import org.workcraft.dom.Connection;
+import org.workcraft.dom.Container;
+import org.workcraft.dom.Node;
 import org.workcraft.dom.visual.VisualComponent;
+import org.workcraft.dom.visual.VisualNode;
+import org.workcraft.exceptions.InvalidConnectionException;
 import org.workcraft.plugins.petri.VisualPlace;
+import org.workcraft.plugins.petri.VisualReadArc;
+import org.workcraft.plugins.petri.VisualReplicaPlace;
 import org.workcraft.plugins.stg.SignalTransition.Direction;
+import org.workcraft.util.Hierarchy;
 import org.workcraft.plugins.stg.VisualSignalTransition;
 import org.workcraft.plugins.stg.VisualStg;
 
@@ -15,6 +25,7 @@ public class ConceptsLayout {
     private static final double yDiff = 2.5;
     private static final double instanceDiff = 1.0;
     private static final double centreXDiff = (xDiff * 2) + 2;
+    private static final double replicaDiff = 1.5;
 
     public static void layout(VisualStg visualStg) {
         HashMap<String, HashSet<VisualComponent>> nodeMap = groupBySignal(visualStg);
@@ -58,7 +69,7 @@ public class ConceptsLayout {
             centre.setLocation(centre.getX() + centreXDiff, centre.getY());
         }
 
-
+        addReplicaPlaces(visualStg);
     }
 
     private static HashMap<String, HashSet<VisualComponent>> groupBySignal(VisualStg visualStg) {
@@ -88,6 +99,64 @@ public class ConceptsLayout {
         }
 
         return nodeMap;
+    }
+
+    private static void addReplicaPlaces(VisualStg visualStg) {
+        Collection<VisualSignalTransition> transitions = visualStg.getVisualSignalTransitions();
+        Container container = Hierarchy.getNearestContainer(new HashSet<Node>(transitions));
+
+        for (VisualSignalTransition t : transitions) {
+            HashSet<VisualReplicaPlace> replicas = new HashSet<>();
+            ArrayList<VisualReplicaPlace> left = new ArrayList<>();
+            ArrayList<VisualReplicaPlace> right = new ArrayList<>();
+            boolean side = false;
+            for (Connection c : visualStg.getConnections(t)) {
+                if (c instanceof VisualReadArc) {
+                    VisualReadArc a = (VisualReadArc) c;
+                    VisualNode first = a.getFirst();
+                    VisualReplicaPlace replicaPlace;
+                    replicaPlace = visualStg.createVisualReplica((VisualPlace) first, container, VisualReplicaPlace.class);
+                    replicas.add(replicaPlace);
+                    if (side) {
+                        right.add(replicaPlace);
+                        side = !side;
+                    } else {
+                        left.add(replicaPlace);
+                        side = !side;
+                    }
+                }
+            }
+            for (int i = 0; i < left.size(); i++) {
+                VisualReplicaPlace r = left.get(i);
+                if (left.size() == 1) {
+                    r.setRootSpacePosition(new Point2D.Double(t.getRootSpaceX() - replicaDiff, t.getRootSpaceY()));
+                } else if (i < (left.size() / 2)) {
+                    r.setRootSpacePosition(new Point2D.Double(t.getRootSpaceX() - replicaDiff, t.getRootSpaceY() + (0.5 * (i + 1))));
+                } else {
+                    r.setRootSpacePosition(new Point2D.Double(t.getRootSpaceX() - replicaDiff, t.getRootSpaceY() - (0.5 * ((i + 1) - (left.size() / 2)))));
+                }
+            }
+
+            for (int i = 0; i < right.size(); i++) {
+                VisualReplicaPlace r = right.get(i);
+                if (right.size() == 1) {
+                    r.setRootSpacePosition(new Point2D.Double(t.getRootSpaceX() + replicaDiff, t.getRootSpaceY()));
+                } else if (i < (right.size() / 2)) {
+                    r.setRootSpacePosition(new Point2D.Double(t.getRootSpaceX() + replicaDiff, t.getRootSpaceY() + (0.5 * (i + 1))));
+                } else {
+                    r.setRootSpacePosition(new Point2D.Double(t.getRootSpaceX() + replicaDiff, t.getRootSpaceY() - (0.5 * ((i + 1) - (right.size() / 2)))));
+                }
+            }
+
+            for (VisualReplicaPlace r : replicas) {
+                try {
+                    visualStg.remove(visualStg.getConnection(r.getMaster(), t));
+                    visualStg.connectUndirected(r, t);
+                } catch (InvalidConnectionException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 
 }
