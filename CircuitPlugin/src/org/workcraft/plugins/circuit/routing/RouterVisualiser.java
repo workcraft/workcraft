0b39c09a32e5771706399c01f03bb84c1d2a1b8f
Danil Sokolov <danilovesky@gmail.com>
2017-05-04 22:58:01 +0100

    Prettify debug output for circuit routing

--- CircuitPlugin/src/org/workcraft/plugins/circuit/routing/RouterVisualiser.java
+++ CircuitPlugin/src/org/workcraft/plugins/circuit/routing/RouterVisualiser.java
@@ -3,7 +3,6 @@ package org.workcraft.plugins.circuit.routing;
 import java.awt.BasicStroke;
 import java.awt.Color;
 import java.awt.Graphics2D;
-import java.awt.geom.Line2D;
 import java.awt.geom.Path2D;
 import java.awt.geom.Point2D;
 import java.awt.geom.Rectangle2D;
@@ -16,8 +15,6 @@ import org.workcraft.plugins.circuit.routing.basic.CoordinateOrientation;
 import org.workcraft.plugins.circuit.routing.basic.Line;
 import org.workcraft.plugins.circuit.routing.basic.Point;
 import org.workcraft.plugins.circuit.routing.basic.Rectangle;
-import org.workcraft.plugins.circuit.routing.basic.RouterConnection;
-import org.workcraft.plugins.circuit.routing.basic.RouterPort;
 import org.workcraft.plugins.circuit.routing.impl.Route;
 import org.workcraft.plugins.circuit.routing.impl.Router;
 import org.workcraft.plugins.circuit.routing.impl.RouterCells;
@@ -27,9 +24,31 @@ public class RouterVisualiser {
     public static void drawEverything(Router router, Graphics2D g) {
         drawBlocks(router, g);
         drawSegments(router, g);
-        drawCells(router, g);
-        drawConnections(router, g);
         drawRoutes(router, g);
+        drawCells(router, g);
+    }
+
+    public static void drawBlocks(Router router, Graphics2D g) {
+        float[] pattern = {0.05f, 0.05f};
+        g.setStroke(new BasicStroke(0.1f * (float) CircuitSettings.getBorderWidth(),
+                BasicStroke.CAP_BUTT, BasicStroke.JOIN_MITER, 1.0f, pattern, 0.0f));
+
+        g.setColor(Color.GREEN.darker());
+        for (Rectangle rec : router.getCoordinatesRegistry().blocked) {
+            Rectangle2D drec = new Rectangle2D.Double(rec.getX(), rec.getY(), rec.getWidth(), rec.getHeight());
+            g.draw(drec);
+        }
+    }
+
+    public static void drawSegments(Router router, Graphics2D g) {
+        g.setStroke(new BasicStroke(0.6f * (float) CircuitSettings.getBorderWidth()));
+        g.setColor(Color.GREEN.darker());
+        for (Line registeredSegment : router.getObstacles().getSegments()) {
+            Path2D shape = new Path2D.Double();
+            shape.moveTo(registeredSegment.getX1(), registeredSegment.getY1());
+            shape.lineTo(registeredSegment.getX2(), registeredSegment.getY2());
+            g.draw(shape);
+        }
     }
 
     public static void drawRoutes(Router router, Graphics2D g) {
@@ -44,19 +63,71 @@ public class RouterVisualiser {
             for (Point routePoint : route.getPoints()) {
                 routeSegments.lineTo(routePoint.getX(), routePoint.getY());
             }
-            g.setColor(Color.RED);
+            g.setColor(Color.GREEN);
+            g.setStroke(new BasicStroke(0.1f * (float) CircuitSettings.getBorderWidth()));
             g.draw(routeSegments);
         }
     }
 
-    public static void drawConnections(Router router, Graphics2D g) {
-        for (RouterConnection connection : router.getObstacles().getConnections()) {
-            RouterPort src = connection.getSource();
-            RouterPort dest = connection.getDestination();
-            Line2D line = new Line2D.Double(src.getLocation().getX(), src.getLocation().getY(),
-                    dest.getLocation().getX(), dest.getLocation().getY());
-            g.setColor(Color.RED);
-            g.draw(line);
+    public static void drawCells(Router router, Graphics2D g) {
+        RouterCells rcells = router.getCoordinatesRegistry().getRouterCells();
+        int[][] cells = rcells.cells;
+        int y = 0;
+        for (Coordinate dy : router.getCoordinatesRegistry().getYCoordinates()) {
+            int x = 0;
+            for (Coordinate dx : router.getCoordinatesRegistry().getXCoordinates()) {
+                boolean isBusy = (cells[x][y] & CellState.BUSY) > 0;
+                boolean isVerticalPrivate = (cells[x][y] & CellState.VERTICAL_PUBLIC) == 0;
+                boolean isHorizontalPrivate = (cells[x][y] & CellState.HORIZONTAL_PUBLIC) == 0;
+
+                boolean isVerticalBlock = (cells[x][y] & CellState.VERTICAL_BLOCK) != 0;
+                boolean isHorizontalBlock = (cells[x][y] & CellState.HORIZONTAL_BLOCK) != 0;
+
+                g.setStroke(new BasicStroke(0.6f * (float) CircuitSettings.getBorderWidth()));
+                Path2D shape = new Path2D.Double();
+                if (isBusy) {
+                    g.setColor(Color.RED);
+                    shape.moveTo(dx.getValue() - 0.1, dy.getValue() - 0.1);
+                    shape.lineTo(dx.getValue() + 0.1, dy.getValue() + 0.1);
+                    shape.moveTo(dx.getValue() + 0.1, dy.getValue() - 0.1);
+                    shape.lineTo(dx.getValue() - 0.1, dy.getValue() + 0.1);
+                    g.draw(shape);
+                } else {
+                    if (isVerticalPrivate) {
+                        shape = new Path2D.Double();
+                        g.setColor(Color.BLUE.darker());
+                        shape.moveTo(dx.getValue(), dy.getValue() - 0.1);
+                        shape.lineTo(dx.getValue(), dy.getValue() + 0.1);
+                        g.draw(shape);
+                    }
+
+                    if (isHorizontalPrivate) {
+                        shape = new Path2D.Double();
+                        g.setColor(Color.BLUE.darker());
+                        shape.moveTo(dx.getValue() - 0.1, dy.getValue());
+                        shape.lineTo(dx.getValue() + 0.1, dy.getValue());
+                        g.draw(shape);
+                    }
+
+                    if (isVerticalBlock) {
+                        shape = new Path2D.Double();
+                        g.setColor(Color.RED);
+                        shape.moveTo(dx.getValue(), dy.getValue() - 0.1);
+                        shape.lineTo(dx.getValue(), dy.getValue() + 0.1);
+                        g.draw(shape);
+                    }
+
+                    if (isHorizontalBlock) {
+                        shape = new Path2D.Double();
+                        g.setColor(Color.RED);
+                        shape.moveTo(dx.getValue() - 0.1, dy.getValue());
+                        shape.lineTo(dx.getValue() + 0.1, dy.getValue());
+                        g.draw(shape);
+                    }
+                }
+                x++;
+            }
+            y++;
         }
     }
 
@@ -105,86 +176,6 @@ public class RouterVisualiser {
             }
             g.setStroke(new BasicStroke(0.5f * (float) CircuitSettings.getBorderWidth()));
             g.draw(grid);
-        }
-
-    }
-
-    public static void drawCells(Router router, Graphics2D g) {
-        RouterCells rcells = router.getCoordinatesRegistry().getRouterCells();
-        int[][] cells = rcells.cells;
-        int y = 0;
-        for (Coordinate dy : router.getCoordinatesRegistry().getYCoordinates()) {
-            int x = 0;
-            for (Coordinate dx : router.getCoordinatesRegistry().getXCoordinates()) {
-                boolean isBusy = (cells[x][y] & CellState.BUSY) > 0;
-                boolean isVerticalPrivate = (cells[x][y] & CellState.VERTICAL_PUBLIC) == 0;
-                boolean isHorizontalPrivate = (cells[x][y] & CellState.HORIZONTAL_PUBLIC) == 0;
-
-                boolean isVerticalBlock = (cells[x][y] & CellState.VERTICAL_BLOCK) != 0;
-                boolean isHorizontalBlock = (cells[x][y] & CellState.HORIZONTAL_BLOCK) != 0;
-
-                Path2D shape = new Path2D.Double();
-                if (isBusy) {
-                    g.setColor(Color.RED);
-                    shape.moveTo(dx.getValue() - 0.1, dy.getValue() - 0.1);
-                    shape.lineTo(dx.getValue() + 0.1, dy.getValue() + 0.1);
-                    shape.moveTo(dx.getValue() + 0.1, dy.getValue() - 0.1);
-                    shape.lineTo(dx.getValue() - 0.1, dy.getValue() + 0.1);
-                    g.draw(shape);
-                } else {
-                    if (isVerticalPrivate) {
-                        shape = new Path2D.Double();
-                        g.setColor(Color.MAGENTA.darker());
-                        shape.moveTo(dx.getValue(), dy.getValue() - 0.1);
-                        shape.lineTo(dx.getValue(), dy.getValue() + 0.1);
-                        g.draw(shape);
-                    }
-
-                    if (isHorizontalPrivate) {
-                        shape = new Path2D.Double();
-                        g.setColor(Color.MAGENTA.darker());
-                        shape.moveTo(dx.getValue() - 0.1, dy.getValue());
-                        shape.lineTo(dx.getValue() + 0.1, dy.getValue());
-                        g.draw(shape);
-                    }
-
-                    if (isVerticalBlock) {
-                        shape = new Path2D.Double();
-                        g.setColor(Color.RED);
-                        shape.moveTo(dx.getValue(), dy.getValue() - 0.1);
-                        shape.lineTo(dx.getValue(), dy.getValue() + 0.1);
-                        g.draw(shape);
-                    }
-
-                    if (isHorizontalBlock) {
-                        shape = new Path2D.Double();
-                        g.setColor(Color.RED);
-                        shape.moveTo(dx.getValue() - 0.1, dy.getValue());
-                        shape.lineTo(dx.getValue() + 0.1, dy.getValue());
-                        g.draw(shape);
-                    }
-                }
-                x++;
-            }
-            y++;
-        }
-    }
-
-    public static void drawSegments(Router router, Graphics2D g) {
-        g.setColor(Color.BLUE.darker());
-        for (Line registeredSegment : router.getObstacles().getSegments()) {
-            Path2D shape = new Path2D.Double();
-            shape.moveTo(registeredSegment.getX1(), registeredSegment.getY1());
-            shape.lineTo(registeredSegment.getX2(), registeredSegment.getY2());
-            g.draw(shape);
-        }
-    }
-
-    public static void drawBlocks(Router router, Graphics2D g) {
-        g.setColor(Color.BLUE.darker());
-        for (Rectangle rec : router.getCoordinatesRegistry().blocked) {
-            Rectangle2D drec = new Rectangle2D.Double(rec.getX(), rec.getY(), rec.getWidth(), rec.getHeight());
-            g.draw(drec);
         }
     }
 
