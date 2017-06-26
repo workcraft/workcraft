0e1d3cfea3a155682c898c0a970f7f56f2fd6739
Danil Sokolov <danilovesky@gmail.com>
2017-06-26 19:13:49 +0100

    Fix sequence of events on tool activation

--- WorkcraftCore/src/org/workcraft/gui/Toolbox.java
+++ WorkcraftCore/src/org/workcraft/gui/Toolbox.java
@@ -206,18 +206,18 @@ public class Toolbox implements ToolProvider, GraphEditorKeyListener {
         if (tracker != null) {
             tracker.track(tool);
         }
+        // Setup and activate the selected tool (before updating Property editor and Tool controls).
         selectedTool = tool;
         setToolButtonSelection(selectedTool, true);
-
-        editor.updateToolsView();
+        selectedTool.setup(editor);
+        selectedTool.activated(editor);
+        // Update the content of Property editor (first) and Tool controls (second).
         editor.updatePropertyView();
-
+        editor.updateToolsView();
+        // Update visibility of Property editor and Tool controls.
         final Framework framework = Framework.getInstance();
         final MainWindow mainWindow = framework.getMainWindow();
         mainWindow.updateDockableWindowVisibility();
-
-        selectedTool.setup(editor);
-        selectedTool.activated(editor);
     }
 
     public void setToolsForModel(JToolBar toolbar) {
