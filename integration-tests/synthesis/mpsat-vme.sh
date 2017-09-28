45e0d481b4fa758ac06da4e81450ca9c3a573368
Danil Sokolov <danilovesky@gmail.com>
2017-09-28 22:27:49 +0100

    Cross-platform use of `sed -i` with .bak extension

--- integration-tests/synthesis/mpsat-vme.sh
+++ integration-tests/synthesis/mpsat-vme.sh
@@ -7,7 +7,9 @@ script_file=${test_dir}/mpsat-vme.js
 # Filter out fanin/fanout statistics as it often changes on different runs
 for f in ${test_dir}/mpsat-vme-*.circuit.stat; do
     [[ -f $f ]] || continue
-    sed  -n -i '/Max fanin \/ fanout/!p' $f
-    sed  -n -i '/Fanin distribution/!p' $f
-    sed  -n -i '/Fanout distribution/!p' $f
+    # OS X `sed -i` requires backup extension, e.g. `sed -i.bak`
+    sed  -n -i.bak '/Max fanin \/ fanout/!p' $f
+    sed  -n -i.bak '/Fanin distribution/!p' $f
+    sed  -n -i.bak '/Fanout distribution/!p' $f
+    rm -f ${f}.bak
 done
