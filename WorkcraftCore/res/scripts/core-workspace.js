44a2067fb876063f99d6ad679c74ef520312e07d
Danil Sokolov <danilovesky@gmail.com>
2017-11-19 22:19:11 +0000

    JS iterator over loaded works and model properties
    Closes #762, closes #759

---
+++ WorkcraftCore/res/scripts/core-workspace.js
@@ -0,0 +1,32 @@
+// Workspace operations
+
+framework.addJavaScriptHelp("getWorks", "",
+    "return an iterable array of loaded works");
+
+function getWorks() {
+    return framework.getWorkspace().getWorks().toArray();
+}
+
+
+framework.addJavaScriptHelp("getWorkFile", "work",
+    "return a file object for the specified work");
+
+function getWorkFile(work) {
+    return work.getFile();
+}
+
+
+framework.addJavaScriptHelp("getModelDescriptor", "work",
+    "return a model descriptor string for the specified work");
+
+function getModelDescriptor(work) {
+    return work.getModelEntry().getDescriptor().getDisplayName();
+}
+
+
+framework.addJavaScriptHelp("getModelTitle", "work",
+    "return a model title string for the specified work");
+
+function getModelTitle(work) {
+    return work.getModelEntry().getModel().getTitle();
+}
