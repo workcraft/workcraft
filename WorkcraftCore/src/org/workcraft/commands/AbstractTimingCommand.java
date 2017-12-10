18f169343d050689ad10f64b65a16fb5a972c414
Danil Sokolov <danilovesky@gmail.com>
2017-12-10 21:10:15 +0000

    Promote Timing commands to the main menu

---
+++ WorkcraftCore/src/org/workcraft/commands/AbstractTimingCommand.java
@@ -0,0 +1,22 @@
+package org.workcraft.commands;
+
+import org.workcraft.MenuOrdering;
+
+public abstract class AbstractTimingCommand implements ScriptableCommand<Boolean>, MenuOrdering {
+
+    @Override
+    public final String getSection() {
+        return "!Timing"; // 0 spaces - positions 5th
+    }
+
+    @Override
+    public int getPriority() {
+        return 0;
+    }
+
+    @Override
+    public Position getPosition() {
+        return null;
+    }
+
+}
