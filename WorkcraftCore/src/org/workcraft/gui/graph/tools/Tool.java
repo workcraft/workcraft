457c0600ff2d45cef43ed5e19fdbbae140cf5bb7
Danil Sokolov <danilovesky@gmail.com>
2018-04-12 23:40:07 +0100

    Enable registration of Tools directly from Modules

--- WorkcraftCore/src/org/workcraft/gui/graph/tools/Tool.java
+++ WorkcraftCore/src/org/workcraft/gui/graph/tools/Tool.java
@@ -4,10 +4,13 @@ import java.awt.Cursor;
 
 import javax.swing.Icon;
 
+import org.workcraft.workspace.WorkspaceEntry;
+
 public interface Tool {
     String getLabel();
     Icon getIcon();
     int getHotKeyCode();
     Cursor getCursor(boolean menuKeyDown, boolean shiftKeyDown, boolean altKeyDown);
     boolean requiresButton();
+    boolean isApplicableTo(WorkspaceEntry we);
 }
