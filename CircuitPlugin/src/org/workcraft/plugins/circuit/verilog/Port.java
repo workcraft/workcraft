bb7957c624a413317cedd159ab25335d32884ef8
Danil Sokolov <danilovesky@gmail.com>
2018-06-08 17:50:47 +0100

    Support for port ranges on Verilog import
    Closes #808

--- CircuitPlugin/src/org/workcraft/plugins/circuit/verilog/Port.java
+++ CircuitPlugin/src/org/workcraft/plugins/circuit/verilog/Port.java
@@ -1,6 +1,9 @@
 package org.workcraft.plugins.circuit.verilog;
 
+import org.workcraft.util.Pair;
+
 public class Port {
+
     public enum Type {
         INPUT("input"),
         OUTPUT("output");
@@ -17,12 +20,37 @@ public class Port {
         }
     }
 
+    public static class Range extends Pair<Integer, Integer> {
+        public Range(Integer first, Integer second) {
+            super(first, second);
+        }
+
+        public int getMin() {
+            return Math.min(getFirst(), getSecond());
+        }
+
+        public int getMax() {
+            return Math.max(getFirst(), getSecond());
+        }
+
+        @Override
+        public String toString() {
+            return "[" + Integer.toString(getFirst()) + ":" + Integer.toString(getSecond()) + "]";
+        }
+    }
+
     public final String name;
     public final Type type;
+    public final Range range;
 
     public Port(String name, Type type) {
+        this(name, type, null);
+    }
+
+    public Port(String name, Type type, Range range) {
         this.name = name;
         this.type = type;
+        this.range = range;
     }
 
     public boolean isInput() {
