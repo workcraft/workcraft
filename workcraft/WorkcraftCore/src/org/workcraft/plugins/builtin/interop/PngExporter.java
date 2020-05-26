8dfa9663a2306f97365f847275c8e1156e7eea28
Danil Sokolov <danilovesky@gmail.com>
2020-05-26 23:27:12 +0100

    Refactor Batik transcoding

--- workcraft/WorkcraftCore/src/org/workcraft/plugins/builtin/interop/PngExporter.java
+++ workcraft/WorkcraftCore/src/org/workcraft/plugins/builtin/interop/PngExporter.java
@@ -1,32 +1,22 @@
 package org.workcraft.plugins.builtin.interop;
 
-import org.apache.batik.transcoder.Transcoder;
-import org.apache.batik.transcoder.TranscoderException;
-import org.apache.batik.transcoder.TranscoderInput;
-import org.apache.batik.transcoder.TranscoderOutput;
 import org.apache.batik.transcoder.image.PNGTranscoder;
 import org.workcraft.dom.Model;
 import org.workcraft.dom.visual.VisualModel;
 import org.workcraft.exceptions.SerialisationException;
 import org.workcraft.interop.Exporter;
+import org.workcraft.utils.BatikUtils;
 
-import java.io.IOException;
-import java.io.InputStream;
 import java.io.OutputStream;
 
 public class PngExporter implements Exporter {
 
     @Override
-    public void export(Model model, OutputStream out) throws IOException, SerialisationException {
-        InputStream svg = SvgExportUtils.streamTripleVote(model);
-        Transcoder transcoder = new PNGTranscoder();
-        TranscoderInput transcoderInput = new TranscoderInput(svg);
-        TranscoderOutput transcoderOutput = new TranscoderOutput(out);
-        try {
-            transcoder.transcode(transcoderInput, transcoderOutput);
-        } catch (TranscoderException e) {
-            throw new SerialisationException(e);
+    public void export(Model model, OutputStream out) throws SerialisationException {
+        if (!(model instanceof VisualModel)) {
+            throw new SerialisationException("Non-visual model cannot be exported as PNG file.");
         }
+        BatikUtils.transcode((VisualModel) model, out, new PNGTranscoder());
     }
 
     @Override
---
+++ workcraft/WorkcraftCore/src/org/workcraft/utils/BatikUtils.java
@@ -0,0 +1,65 @@
+package org.workcraft.utils;
+
+import org.apache.batik.svggen.SVGGraphics2D;
+import org.apache.batik.transcoder.Transcoder;
+import org.apache.batik.transcoder.TranscoderException;
+import org.apache.batik.transcoder.TranscoderInput;
+import org.apache.batik.transcoder.TranscoderOutput;
+import org.w3c.dom.Document;
+import org.workcraft.dom.visual.VisualGroup;
+import org.workcraft.dom.visual.VisualModel;
+import org.workcraft.exceptions.SerialisationException;
+import org.workcraft.gui.tools.Decorator;
+
+import javax.xml.parsers.ParserConfigurationException;
+import java.awt.*;
+import java.awt.geom.Rectangle2D;
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+
+public class BatikUtils {
+
+    private static final double SCALE_FACTOR = 50.0;
+
+    public static void transcode(VisualModel model, OutputStream out)
+            throws SerialisationException {
+
+        BatikUtils.generateSvgGraphics(model, out);
+    }
+
+    public static void transcode(VisualModel model, OutputStream out, Transcoder transcoder)
+            throws SerialisationException {
+
+        ByteArrayOutputStream bufOut = new ByteArrayOutputStream();
+        BatikUtils.generateSvgGraphics(model, bufOut);
+        ByteArrayInputStream bufIn = new ByteArrayInputStream(bufOut.toByteArray());
+        TranscoderInput transcoderInput = new TranscoderInput(bufIn);
+        TranscoderOutput transcoderOutput = new TranscoderOutput(out);
+        try {
+            transcoder.transcode(transcoderInput, transcoderOutput);
+        } catch (TranscoderException e) {
+            throw new SerialisationException(e);
+        }
+    }
+
+    private static void generateSvgGraphics(VisualModel model, OutputStream out) throws SerialisationException {
+        try {
+            Document doc = XmlUtils.createDocument();
+            SVGGraphics2D g2d = new SVGGraphics2D(doc);
+            g2d.setUnsupportedAttributes(null);
+            g2d.scale(SCALE_FACTOR, SCALE_FACTOR);
+            VisualGroup visualGroup = (VisualGroup) model.getRoot();
+            Rectangle2D bounds = visualGroup.getBoundingBoxInLocalSpace();
+            g2d.translate(-bounds.getMinX(), -bounds.getMinY());
+            int canvasWidth = (int) (bounds.getWidth() * SCALE_FACTOR);
+            int canvasHeight = (int) (bounds.getHeight() * SCALE_FACTOR);
+            g2d.setSVGCanvasSize(new Dimension(canvasWidth, canvasHeight));
+            model.draw(g2d, Decorator.Empty.INSTANCE);
+            g2d.stream(new OutputStreamWriter(out, StandardCharsets.UTF_8));
+            out.flush();
+        } catch (ParserConfigurationException | IOException e) {
+            throw new SerialisationException(e);
+        }
+    }
+
+}
