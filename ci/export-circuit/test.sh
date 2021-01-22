830e22da10dc8db367384774475ecbb245a96bdd
Danil Sokolov <danilovesky@gmail.com>
2021-01-22 20:44:00 +0000

    Convert CRLF EOL into CR for diff comparison

--- ci/export-circuit/test.sh
+++ ci/export-circuit/test.sh
@@ -5,21 +5,26 @@ result_file=${test_dir}/vme-tm.circuit.txt
 function report_file_header() {
     file_name=$1
     header_size=$2
-    options=$3
+    clean_option=${3:keep} # [keep], remove
+    eol_option=${4:preserve} # [preserve], unix
+    if [[ "$eol_option" == "unix" ]]; then
+        sed -i.bak 's/\\r$//g' ${file_name}
+        rm -f ${file_name}.bak
+    fi
     if [[ -e $file_name ]]; then
         header=$(head -c $header_size $file_name)
     fi
     echo "$(basename $file_name):" >> ${result_file}
     echo "${header}" >> ${result_file}
     echo >> ${result_file}
-    if [[ "$options" == "remove" ]]; then
+    if [[ "$clean_option" == "remove" ]]; then
         rm -f $file_name
     fi
 }
 
 rm -f ${result_file}
-report_file_header ${test_dir}/vme-tm.circuit.v 144 keep
-report_file_header ${test_dir}/vme-tm.circuit.svg 155 remove
+report_file_header ${test_dir}/vme-tm.circuit.v 144 keep unix
+report_file_header ${test_dir}/vme-tm.circuit.svg 155 remove unix
 report_file_header ${test_dir}/vme-tm.circuit.png 8 remove
 report_file_header ${test_dir}/vme-tm.circuit.pdf 9 remove
 report_file_header ${test_dir}/vme-tm.circuit.eps 24 remove
