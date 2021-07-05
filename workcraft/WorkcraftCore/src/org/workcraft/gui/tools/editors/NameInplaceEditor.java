71a9bdbe983dc4316450dbd1623097981d0b95d7
Danil Sokolov <danilovesky@gmail.com>
2021-07-05 17:20:51 +0100

    Validate Circuit port name if using inplace editor

--- workcraft/WorkcraftCore/src/org/workcraft/gui/tools/editors/NameInplaceEditor.java
+++ workcraft/WorkcraftCore/src/org/workcraft/gui/tools/editors/NameInplaceEditor.java
@@ -1,5 +1,6 @@
 package org.workcraft.gui.tools.editors;
 
+import org.workcraft.dom.references.Identifier;
 import org.workcraft.dom.visual.VisualComponent;
 import org.workcraft.dom.visual.VisualModel;
 import org.workcraft.exceptions.ArgumentException;
@@ -8,19 +9,28 @@ import org.workcraft.utils.DialogUtils;
 import org.workcraft.workspace.WorkspaceEntry;
 
 public class NameInplaceEditor extends AbstractInplaceEditor {
+
     private final VisualComponent component;
     private final VisualModel model;
+    private final boolean validate;
 
     public NameInplaceEditor(GraphEditor editor, VisualComponent component) {
+        this(editor, component, false);
+    }
+    public NameInplaceEditor(GraphEditor editor, VisualComponent component, boolean validate) {
         super(editor, component);
         this.component = component;
         this.model = editor.getModel();
+        this.validate = validate;
     }
 
     @Override
     public void processResult(String text) {
-        WorkspaceEntry we = getEditor().getWorkspaceEntry();
         try {
+            if (validate) {
+                Identifier.validate(text);
+            }
+            WorkspaceEntry we = getEditor().getWorkspaceEntry();
             we.captureMemento();
             model.setMathName(component, text);
             we.saveMemento();
