090ba78cec96d9c66388ab4e4f76d0eaddd86bee
Danil Sokolov <danilovesky@gmail.com>
2022-05-16 14:03:39 +0100

    Improve config CLI and ENV functionality
    Closes #1354

--- ci/config/test.sh
+++ ci/config/test.sh
@@ -1,3 +1,20 @@
-# Test local config file
-cp -f ${test_dir}/config-local.xml.min ${test_dir}/config-local.xml
-./gradlew run --args="-nogui -config:config-local.xml -dir:${test_dir} -exec:'exit();'" >${log_file}
+# Base config defined via CLI
+cp -f ${test_dir}/config-base-cli.xml.min ${test_dir}/config-base-cli.xml
+./gradlew run --args="-nogui -config:config-base-cli.xml -dir:${test_dir} -exec:'exit();'" >${log_file}
+
+# Additional config and new resultant config defined via CLI
+cp -f ${test_dir}/config-new-cli.xml.min ${test_dir}/config-additional-cli.xml
+./gradlew run --args="-nogui -noconfig-load -config:config-new-cli.xml -config-add:config-additional-cli.xml -dir:${test_dir} -exec:'exit();'" >${log_file}
+rm ${test_dir}/config-additional-cli.xml
+
+# Base config defined via ENV
+cp -f ${test_dir}/config-base-env.xml.min ${test_dir}/config-base-env.xml
+export WORKCRAFT_CONFIG=config-base-env.xml
+./gradlew run --args="-nogui -dir:${test_dir} -exec:'exit();'" >${log_file}
+
+# Additional config and new resultant config defined via ENV
+cp -f ${test_dir}/config-new-env.xml.min ${test_dir}/config-additional-env.xml
+export WORKCRAFT_CONFIG=config-new-env.xml
+export WORKCRAFT_CONFIG_ADD=config-additional-env.xml
+./gradlew run --args="-nogui -noconfig-load -dir:${test_dir} -exec:'exit();'" >${log_file}
+rm ${test_dir}/config-additional-env.xml
--- workcraft/WorkcraftCore/src/org/workcraft/Options.java
+++ workcraft/WorkcraftCore/src/org/workcraft/Options.java
@@ -13,32 +13,49 @@ public class Options {
     private static final String FLAG_PREFIX = "-";
     private static final String DIR_OPTION = "-dir:";
     private static final String CONFIG_OPTION = "-config:";
+    private static final String CONFIG_ADD_OPTION = "-config-add:";
     private static final String EXEC_OPTION = "-exec:";
     private static final String PORT_OPTION = "-port:";
     private static final String NOGUI_OPTION = "-nogui";
     private static final String NOCONFIG_OPTION = "-noconfig";
+    private static final String NOCONFIG_LOAD_OPTION = "-noconfig-load";
+    private static final String NOCONFIG_SAVE_OPTION = "-noconfig-save";
     private static final String VERSION_OPTION = "-version";
     private static final String HELP_OPTION = "-help";
 
-    private static final String HELP = "Usage:  workcraft [OPTIONS] [FILES]\n" +
-            "OPTIONS - space-separated list of the following options:\n" +
-            "  " + DIR_OPTION + "DIR       path to the working directory\n" +
-            "  " + CONFIG_OPTION + "CONFIG user-defined config file (relative to DIR)\n" +
-            "  " + EXEC_OPTION + "SCRIPT   JavaScript file (relative to DIR) or one-liner to execute on startup\n" +
-            "  " + PORT_OPTION + "PORT     reuse running instance on PORT to open FILES\n" +
-            "  " + NOGUI_OPTION + "         run in console mode\n" +
-            "  " + NOCONFIG_OPTION + "      use default settings without overwriting user config\n" +
-            "  " + VERSION_OPTION + "       report the version information and exit\n" +
-            "  " + HELP_OPTION + "          display this help message and exit\n" +
-            "FILES - space-separated list of work files to open (relative to DIR) or arguments for SCRIPT\n";
+    private static final String CONFIG_ENV = "WORKCRAFT_CONFIG";
+    private static final String CONFIG_ADD_ENV = "WORKCRAFT_CONFIG_ADD";
+
+    private static final String HELP = "Usage:  workcraft [OPTIONS] [FILES]"
+            + "\nOPTIONS - space-separated list of the following options:"
+            + "\n  " + DIR_OPTION + "DIR           path to the working directory"
+            + "\n  " + CONFIG_OPTION + "CONFIG     user config file (default is global config.xml)"
+            + "\n  " + CONFIG_ADD_OPTION + "CONFIG additional read-only config file to override user config settings"
+            + "\n  " + EXEC_OPTION + "SCRIPT       JavaScript file or one-liner to execute on startup"
+            + "\n  " + PORT_OPTION + "PORT         reuse running instance on PORT to open FILES"
+            + "\n  " + NOGUI_OPTION + "             run in console mode"
+            + "\n  " + NOCONFIG_LOAD_OPTION + "     use default settings instead of loading them from user config"
+            + "\n  " + NOCONFIG_SAVE_OPTION + "     do not overwriting user config on exit"
+            + "\n  " + NOCONFIG_OPTION + "          use default settings and do not overwriting user config"
+            + "\n  " + VERSION_OPTION + "           report the version information and exit"
+            + "\n  " + HELP_OPTION + "              display this help message and exit"
+            + "\nFILES - space-separated list of work files to open or arguments for SCRIPT"
+            + "\n"
+            + "\nNote that file path parameters CONFIG, SCRIPT and FILES are relative to the working directory."
+            + "\n"
+            + "\nUse environment variables " + CONFIG_ENV + " and " + CONFIG_ADD_ENV + " as lower priority"
+            + "\nalternatives to command line options " + CONFIG_OPTION + " and " + CONFIG_ADD_OPTION + " respectively."
+            + "\n";
 
     private final Collection<String> paths;
     private final File directory;
     private final String config;
+    private final String configAddition;
     private final String script;
     private final Integer port;
     private final boolean noGuiFlag;
-    private final boolean noConfigFlag;
+    private final boolean noConfigLoadFlag;
+    private final boolean noConfigSaveFlag;
     private final boolean helpFlag;
     private final boolean versionFlag;
 
@@ -52,20 +69,31 @@ public class Options {
                 .collect(Collectors.toList());
 
         directory = getOptionLastValue(args, DIR_OPTION, File::new);
-        config = getOptionLastValue(args, CONFIG_OPTION, String::new);
-        script = getOptionLastValue(args, EXEC_OPTION, String::new);
+        config = getOptionOrEnvLastValue(args, CONFIG_OPTION, CONFIG_ENV);
+        configAddition = getOptionOrEnvLastValue(args, CONFIG_ADD_OPTION, CONFIG_ADD_ENV);
+        script = getOptionLastValue(args, EXEC_OPTION);
         port = getOptionLastValue(args, PORT_OPTION, Integer::valueOf);
         noGuiFlag = args.contains(NOGUI_OPTION);
-        noConfigFlag = args.contains(NOCONFIG_OPTION);
+        noConfigLoadFlag = args.contains(NOCONFIG_LOAD_OPTION) || args.contains(NOCONFIG_OPTION);
+        noConfigSaveFlag = args.contains(NOCONFIG_SAVE_OPTION) || args.contains(NOCONFIG_OPTION);
         helpFlag = args.contains(HELP_OPTION);
         versionFlag = args.contains(VERSION_OPTION);
     }
 
-    private <T> T getOptionLastValue(List<String> args, String prefix, Function<String, T> transformer) {
+    private String getOptionOrEnvLastValue(List<String> args, String optionPrefix, String envName) {
+        String optionLastValue = getOptionLastValue(args, optionPrefix);
+        return optionLastValue == null ? System.getenv(envName) : optionLastValue;
+    }
+
+    private <T> T getOptionLastValue(List<String> args, String optionPrefix, Function<String, T> transformer) {
+        String optionLastValue = getOptionLastValue(args, optionPrefix);
+        return optionLastValue == null ? null : transformer.apply(optionLastValue);
+    }
+
+    private String getOptionLastValue(List<String> args, String optionPrefix) {
         return args.stream()
-                .filter(arg -> (arg != null) && arg.startsWith(prefix))
-                .map(arg -> arg.substring(prefix.length()))
-                .map(transformer)
+                .filter(arg -> (arg != null) && arg.startsWith(optionPrefix))
+                .map(arg -> arg.substring(optionPrefix.length()))
                 .reduce((first, second) -> second)
                 .orElse(null);
     }
@@ -82,6 +110,10 @@ public class Options {
         return config;
     }
 
+    public String getConfigAddition() {
+        return configAddition;
+    }
+
     public String getScript() {
         return script;
     }
@@ -94,8 +126,12 @@ public class Options {
         return noGuiFlag;
     }
 
-    public boolean hasNoConfigFlag() {
-        return noConfigFlag;
+    public boolean hasNoConfigLoadFlag() {
+        return noConfigLoadFlag;
+    }
+
+    public boolean hasNoConfigSaveFlag() {
+        return noConfigSaveFlag;
     }
 
     public boolean hasHelpFlag() {
