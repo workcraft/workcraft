1f9158717afb64650ed5a6809433321bf8864471
Danil Sokolov <danilovesky@gmail.com>
2022-11-25 18:52:20 +0000

    Command to update components from refinements
    Fixes #1396

---
+++ workcraft/CircuitPlugin/src/org/workcraft/plugins/circuit/commands/UpdateComponentInterfaceTransformationCommand.java
@@ -0,0 +1,40 @@
+package org.workcraft.plugins.circuit.commands;
+
+import org.workcraft.dom.references.FileReference;
+import org.workcraft.dom.visual.VisualNode;
+import org.workcraft.plugins.circuit.CircuitPropertyHelper;
+import org.workcraft.plugins.circuit.VisualCircuit;
+import org.workcraft.plugins.circuit.VisualFunctionComponent;
+import org.workcraft.plugins.circuit.utils.RefinementUtils;
+import org.workcraft.workspace.ModelEntry;
+
+public class UpdateComponentInterfaceTransformationCommand extends AbstractComponentTransformationCommand {
+
+    @Override
+    public String getDisplayName() {
+        return "Update interface of components from refinement model (selected or all)";
+    }
+
+    @Override
+    public String getPopupName() {
+        return "Update interface from refinement model";
+    }
+
+    @Override
+    public boolean isEnabled(ModelEntry me, VisualNode node) {
+        return (node instanceof VisualFunctionComponent)
+                && RefinementUtils.hasRefinementCircuit((VisualFunctionComponent) node);
+    }
+
+    @Override
+    public Position getPosition() {
+        return Position.TOP_MIDDLE;
+    }
+
+    @Override
+    public void transformComponent(VisualCircuit circuit, VisualFunctionComponent component) {
+        FileReference refinement = component.getReferencedComponent().getRefinement();
+        CircuitPropertyHelper.setRefinementIfCompatible(circuit, component, refinement);
+    }
+
+}
