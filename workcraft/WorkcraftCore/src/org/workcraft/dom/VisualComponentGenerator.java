a604fdc03ca7fa9969deb3b635e688eb8742d40d
Danil Sokolov <danilovesky@gmail.com>
2023-02-09 17:17:22 +0000

    Remove unused VisualComponentGenerator class

--- workcraft/WorkcraftCore/src/org/workcraft/dom/VisualComponentGenerator.java
+++
@@ -1,9 +0,0 @@
-package org.workcraft.dom;
-
-import org.workcraft.dom.math.MathNode;
-import org.workcraft.dom.visual.VisualComponent;
-
-public interface VisualComponentGenerator {
-    VisualComponent createComponent(MathNode component, Object... constructorParameters);
-
-}
