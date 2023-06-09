2da243626a42f753bd9becdc11ee06a038d884bd
Danil Sokolov <danilovesky@gmail.com>
2023-06-09 12:06:57 +0100

    Rename MUTEX instance from me1 to me

--- workcraft/CircuitPlugin/test-res/org/workcraft/plugins/circuit/mutex-buf.circuit.v
+++ workcraft/CircuitPlugin/test-res/org/workcraft/plugins/circuit/mutex-buf.circuit.v
@@ -5,7 +5,7 @@ module mutex(in1 in2, out1, out2);
     BUF inbuf1 (.I(in1), .O(v1));
     BUF inbuf2 (.I(in2), .O(v2));
 
-    MUTEX me1 (.r1(v1), .g1(w1), .r2(v2), .g2(w2));
+    MUTEX me (.r1(v1), .g1(w1), .r2(v2), .g2(w2));
 
     BUF outbuf1 (.I(w1), .O(out1));
     BUF outbuf2 (.I(w2), .O(out2));
