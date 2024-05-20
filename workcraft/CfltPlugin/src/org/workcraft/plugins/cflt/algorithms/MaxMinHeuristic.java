9324888255b87fcec88f965b0d168876cccdb4cd
IgorWiecz <igorwieczorek9@gmail.com>
2024-05-20 10:57:17 +0100

    unused import removed

--- workcraft/CfltPlugin/src/org/workcraft/plugins/cflt/algorithms/MaxMinHeuristic.java
+++ workcraft/CfltPlugin/src/org/workcraft/plugins/cflt/algorithms/MaxMinHeuristic.java
@@ -1,7 +1,6 @@
 package org.workcraft.plugins.cflt.algorithms;
 
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.stream.Collectors;
