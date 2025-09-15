9c57e03e87300a5b8a413e6e36166ebea57ff62a
Danil Sokolov <danilovesky@gmail.com>
2025-09-15 10:07:54 +0100

    Add customisation of base directory for backend tools

--- workcraft/WorkcraftCore/src/org/workcraft/utils/BackendUtils.java
+++ workcraft/WorkcraftCore/src/org/workcraft/utils/BackendUtils.java
@@ -4,9 +4,10 @@ import java.io.File;
 
 public class BackendUtils {
 
-    private static final String LINUX_TEMPLATE_PATH = "dist/template/linux/";
-    private static final String MACOS_TEMPLATE_PATH = "dist/template/osx/Contents/Resources/";
-    private static final String WINDOWS_TEMPLATE_PATH = "dist\\template\\windows\\";
+    private static final String TEMPLATE_DIR_PATH = "dist/template/";
+    private static final String LINUX_DIR_PATH = "linux/";
+    private static final String MACOS_DIR_PATH = "osx/Contents/Resources/";
+    private static final String WINDOWS_DIR_PATH = "windows/";
 
     private static final String TOOLS_DIR_NAME = "tools";
     private static final String LIBRARIES_DIR_NAME = "libraries";
@@ -34,19 +35,27 @@ public class BackendUtils {
     }
 
     public static String getTemplateLibraryPath(String fileName) {
+        return getTemplateLibraryPath(fileName, TEMPLATE_DIR_PATH);
+    }
+
+    public static String getTemplateLibraryPath(String fileName, String templateDirPath) {
         return switch (DesktopApi.getOs()) {
-            case LINUX -> LINUX_TEMPLATE_PATH + getLibraryPath(fileName);
-            case MACOS -> MACOS_TEMPLATE_PATH + getLibraryPath(fileName);
-            case WINDOWS -> WINDOWS_TEMPLATE_PATH + getLibraryPath(fileName);
+            case LINUX -> templateDirPath + LINUX_DIR_PATH + getLibraryPath(fileName);
+            case MACOS -> templateDirPath + MACOS_DIR_PATH + getLibraryPath(fileName);
+            case WINDOWS -> templateDirPath + WINDOWS_DIR_PATH + getLibraryPath(fileName);
             default -> getLibraryPath(fileName);
         };
     }
 
     public static String getTemplateToolPath(String dirName, String fileName) {
+        return getTemplateToolPath(dirName, fileName, TEMPLATE_DIR_PATH);
+    }
+
+    public static String getTemplateToolPath(String dirName, String fileName, String templateDirPath) {
         return switch (DesktopApi.getOs()) {
-            case LINUX -> LINUX_TEMPLATE_PATH + getToolPath(dirName, fileName);
-            case MACOS -> MACOS_TEMPLATE_PATH + getToolPath(dirName, fileName);
-            case WINDOWS -> WINDOWS_TEMPLATE_PATH + getToolPath(dirName, fileName);
+            case LINUX -> templateDirPath + LINUX_DIR_PATH + getToolPath(dirName, fileName);
+            case MACOS -> templateDirPath + MACOS_DIR_PATH + getToolPath(dirName, fileName);
+            case WINDOWS -> templateDirPath + WINDOWS_DIR_PATH + getToolPath(dirName, fileName);
             default -> getToolPath(dirName, fileName);
         };
     }
